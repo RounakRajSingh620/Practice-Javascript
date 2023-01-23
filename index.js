1545ee1516bc78c8abf43e97ea1ed4d383e713b5
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-01-24 00:16:59 +0530

    Basics of JAVASCRIPT

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 1.Ways to print in JS
- 
+
 //console.log("Hello world");
 // alert("me")
 // document.write("this is document write")
@@ -13,34 +13,34 @@
 // 3. Js Variables
 // What are Variables? -  Containers to store data values
 
-var numbers1= 34;
-var numbers2=56;
+var numbers1 = 34;
+var numbers2 = 56;
 // console.log(numbers1+numbers2);
 
 // 4.Data types in Js
 // Numbers
-var num1=455;
-var num1=56.78;
+var num1 = 455;
+var num1 = 56.78;
 //Strings
-var str1= "This is a string";
-var str2= "This is also a string";
+var str1 = "This is a string";
+var str2 = "This is also a string";
 // objects
-var marks={
-    ravi:34,
-    shumbham:58,
-    raj:56
+var marks = {
+    ravi: 34,
+    shumbham: 58,
+    raj: 56
 }
 console.log(marks);
 
 //boolean
-var a=true;
-var b=false;
+var a = true;
+var b = false;
 // console.log(a,b);
 
 var und;
 // console.log(und);
 
-var n=null;
+var n = null;
 // console.log(n);
 
 /* At a very high level there are two types of data types
@@ -50,21 +50,21 @@ in javascript
 */
 
 //Array 
-var arr=[1,2,3,4,5]
+var arr = [1, 2, 3, 4, 5]
 // console.log(arr)
 
 //OPerators in Javascript
 // Arithmetic Operators
 
-var a=100;
-var b=10;
-console.log("The value of a+b is ",a+b);
-console.log("The value of a-b is ",a-b);
-console.log("The value of a*b is ",a*b);
-console.log("The value of a/b is ",a/b);
+var a = 100;
+var b = 10;
+console.log("The value of a+b is ", a + b);
+console.log("The value of a-b is ", a - b);
+console.log("The value of a*b is ", a * b);
+console.log("The value of a/b is ", a / b);
 
 // Assignment Operators
-var c=b;
+var c = b;
 //c+=2;
 //c -=2; c=c-2;
 // c*=2;
@@ -72,8 +72,8 @@ var c=b;
 //console.log(c);
 
 // Comparison Operators
-var x=34;
-var y=56;
+var x = 34;
+var y = 56;
 // console.log(x==y);
 // console.log(x>=y);
 // console.log(x<=y);
@@ -99,22 +99,24 @@ var y=56;
 //console.log(!true)
 
 // Function in Javascript
-function avg(a,b){
-    c=(a+b)/2;
+function avg(a, b) {
+    c = (a + b) / 2;
     return c;
 }
 // DRY=Do not repeat yourself
-c1=avg (4,6);
-c2=avg(14,16);
+c1 = avg(4, 6);
+c2 = avg(14, 16);
 // console.log(c1,c2);
 
 // Conditional in Javascript
+/*
 var age =4;
 //Single if statement
 if(age >18){
     console.log('You can drink  rasna water');
 }
 // if -else statement
+
 if(age >18){
     console.log('You can drink  rasna water');
 }
@@ -122,9 +124,46 @@ else{
     console.log("You cannot drink rasna water");
 }
 // if-else ladder
+age=25;
 if(age>32){
     console.log("You are not a kid")
 }
 else if(age>26){
     console.log("No child ")
 }
+else if(age>22){
+    console.log("Yes their is no child");
+}
+else if(age>18){
+    console.log("18 children");
+}
+else{
+    console.log("child is their");
+}
+console.log("End of ladder");
+*/
+
+var arr = [1, 2.3, 4, 5, 6, 8];
+// console.log(arr);
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// arr.forEach(function(element){
+//     console.log(element)
+// })
+
+// const ac = 0;
+// ac++;
+// ac=ac+1;
+
+let j = 0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+
+do {
+    console.log(arr[j]);
+    j++;
+} while (j < arr.length);
